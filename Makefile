d0259b601be1b49c4c137f458eabb9b68f246d86
willfordrockwell <ermacprodac@gmail.com>
2019-02-10 02:50:56 +0700

    add makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: main.c
+	gcc -o main.exe main.c
--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 //---------------------------------------------------------------------------
-#include "stdafx.h"
 #include <stdio.h>
-#include <winsock2.h>
-#include <windows.h>
-#include <errno.h>
-#include <unistd.h> 
 #include <string.h>
-#include <sys/types.h> 
-#include <netinet/in.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h> 
 //---------------------------------------------------------------------------
-void checkArgs() {
+void checkArgs(int argc) {	//Num of args
 	if (argc < 2) {
 		printf("Enter argument: 0 for client (Second player), 1 for server (First player)");
 		exit(-1);
@@ -19,24 +15,9 @@ void checkArgs() {
 }
 //---------------------------------------------------------------------------
 void initClient() {
+	char serverIP[16];
 	printf("Enter server's IP (example): ");
-	scanf();
-	
-	//Init socket
-	struct WSAData WS;
-	if (FAILED(WSAStartup(0x202, (WSADATA *)&WS))){
-		//Error
-		printf("Client can NOT initialize WSAStartup, error: %d\n", WSAGetLastError());
-		exit(-3);
-	}
-		
-	//Create windows socket
-	SOCKET sockTCP;
-	if ((sockTCP = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET){
-		//Error
-		printf("Client can NOT create socket, error: %d\n", WSAGetLastError());
-		exit(-4);
-	}
+	scanf("%s", &serverIP);
 }
 //---------------------------------------------------------------------------
 void initServer() {
@@ -44,7 +25,7 @@ void initServer() {
 }
 //---------------------------------------------------------------------------
 int main (int argc, char *argv[]) { //Server: 1 serverPort; Client: 0 serverIP serverPort
-	checkArgs()
+	checkArgs(argc);
 	
 	if (atoi(argv[1]) == 0) {
 		//Client code
@@ -52,7 +33,7 @@ int main (int argc, char *argv[]) { //Server: 1 serverPort; Client: 0 serverIP s
 	}
 	else if (atoi(argv[1]) == 1) {
 		//Server code
-		printf("1");
+		initServer();
 	}
 	else {
 		//WTF code
